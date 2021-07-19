84c3905398eb45f5c77b5deb44985f226595a9a1
Patrick Drechsler <patrick@pdrechsler.de>
2021-07-19 15:57:06 +0200

    ✨ initial commit

---
+++ Demo/CitySearchTests.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Demo
+{
+    public class CitySearchTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
